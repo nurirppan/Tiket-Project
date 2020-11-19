46a4f6d6aa48264c7cfd6f7a306cebad214c32db
Nur Irfan Pangestu <nur.pangestu@indivaragroup.com>
2020-11-19 22:24:06 +0700

    create project

---
+++ Tiket Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tiket Project
+//
+//  Created by Nur Irfan Pangestu on 19/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tiket ProjectTests/Tiket_ProjectTests.swift
@@ -0,0 +1,33 @@
+//
+//  Tiket_ProjectTests.swift
+//  Tiket ProjectTests
+//
+//  Created by Nur Irfan Pangestu on 19/11/20.
+//
+
+import XCTest
+@testable import Tiket_Project
+
+class Tiket_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
